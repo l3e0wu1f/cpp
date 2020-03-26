4195d1aec227db570de7e0b747a4cc37413f1a0e
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2020-03-26 12:14:28 -0400

    Computer Science 1 Projects

--- JoshLewisProj1.cpp
+++ JoshLewisProj1.cpp
@@ -1,146 +1,20 @@
 //
-//  JoshLewisProj1.cpp
+//  JoshLewisProj1
+//  Created by Josh Lewis on 8/20/17.
 //
-//
-//  Created by Josh Lewis on 1/27/18.
-//
-//
+/*  Step 2: No difference is seen in the program's output after comments were added.
+    Step 3: Xcode won't run the program when Cout is capitalized. 
+            Error says: "Use of undeclared identifier 'Cout'; did you mean 'cout'?" */
 
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <cstdlib>
-
 using namespace std;
 
-void revString(char* front, char* rear);
-// Reverses string by swapping values between two pointers
-// Tests for both odd and even strings
-
-string *addEntry(string *dynamicArray, int& size, string newEntry);
-
-string* deleteEntry(string* dynamicArray, int& size, string entryToDelete);
-
-void print(string *dynamicArray, int size);
-
-int main()
-{
-    // Chooses one odd and one even string to reverse
-    char odd[] = "Vader";
-    char even[] = "Yoda";
-    
-    cout << "Before reversal, the odd string is: " << odd << endl;
-    cout << "Before reversal, the even string is: " << even << endl;
-    
-    revString(odd, odd + strlen(odd) - 1);
-    
-    revString(even, even + strlen(even) - 1);
-    
-    cout << "After reversal, the odd string is: " << odd << endl;
-    cout << "After reversal, the even string is: " << even << endl;
-
-    // Creates array and enters some names
-    int size = 5;
-    string *names = new string[size];
-    names[0] = "Luke";
-    names[1] = "Leia";
-    names[2] = "Han";
-    names[3] = "C3PO";
-    names[4] = "R2D2";
-    
-    cout << "Initial list of names: \n";
-    print(names,size);
-    
-    // Add some additional entries
-    names = addEntry(names,size,"Jabba");
-    names = addEntry(names,size,"Greedo");
-    names = addEntry(names,size,"Palpatine");
-    cout << "The list with some added villains is now: \n";
-    print(names,size);
-    
-    // Delete some entries
-    names = deleteEntry(names,size,"Greedo");
-    names = deleteEntry(names,size,"Jabba");
-    cout << "After defeating two villains, the cast of characters remains: \n";
-    print(names,size);
-    
-    //Delete entire array
-    delete [] names;
-    
-    return 0;
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "This is my first C++ program" << endl;
+    system("pause");
 }
 
-void revString(char* front, char* rear)
-{
-    char temp;
-    while(front < rear)
-    {
-        temp = *front;
-        *front = *rear;
-        *rear = temp;
-        front++;
-        rear--;
-    }
-}
+/* I also get an error "command not found" regarding the system call to pause. After some research, I discovered this is OS-specific, and only works in Windows shells. I'm currently using Xcode 4 on Mac. */
 
-string *addEntry(string *dynamicArray, int& size, string newEntry)
-{
-    // Create new dynamic array 1 element longer than dynamicArray
-    // Copy all elements from dynamicArray into new array
-    // Add new entry onto end of new array
-    // Increment size, delete dynamicArray, and return new array
-    
-    string *newArray = new string[size + 1];
-    for(int i = 0; i < size; i++)
-        newArray[i] = dynamicArray[i];
-    newArray[size] = newEntry;
-    size++;
-    delete [] dynamicArray;
-    return newArray;
-}
-
-string* deleteEntry(string* dynamicArray, int& size, string entryToDelete)
-{
-    // Search dynamicArray for entryToDelete
-    // If not found, request to be ignored & dynamicArray returned
-    // If found, create new dynamic array 1 element smaller than dynamicArray
-    // Copy all elements except entryToDelete into new array
-    // Delete dynamicArray, decrement size, and return new array
-    
-    string *result = dynamicArray;
-    int index_of_entry = -1;
-    for(int i = 0; i < size; i++)
-    {
-        if(dynamicArray[i] == entryToDelete){
-            index_of_entry = i;
-            break;
-        }
-    }
-    if(index_of_entry == -1)
-        return result;
-    else
-    {
-        result = new string[size - 1];
-        int index_new_array = 0;
-        for(int i = 0; i < size; i++)
-            if(i != index_of_entry)
-            {
-                result[index_new_array] = dynamicArray[i];
-                index_new_array++;
-            }
-            else
-            {
-                // i == index_of_entry
-            }
-        size--;
-        delete [] dynamicArray;
-    }
-    return result;
-}
-
-void print(string *dynamicArray, int size)
-{
-    for(int i = 0; i < size; i++)
-        cout << i << ": " << dynamicArray[i] << endl;
-    cout << endl;
-}
+/* Question: Why do we not need to include 'return 0;' at the end of program? Since main() function is declared as an integer, I thought the 'return 0;' was necessary. */
--- JoshLewisProj8.cpp
+++ JoshLewisProj8.cpp
@@ -1,120 +1,101 @@
-// This is the program file of Project 8: A template-based class that implements a set of items.
-// Name: Josh Lewis
-// Email: jblewis@mail.fhsu.edu
-// Date: 4/25/2018
+//
+//  JoshLewisProj8.cpp
+//
+//
+//  Created by Josh Lewis on 11/21/17.
+//
+//
 
 #include <iostream>
-#include "JoshLewisProj8Set.h"
+#include <string>
+#include <vector>
+#include <cctype>
 
+using namespace std;
 
-class A
+void get_scores(vector<int> &v);
+// get integer scores from keyboard and store in v.
+// we assume that the user will input scores in range
+// of 0 to 100, inclusive. User enter a negative
+// number to stop the input and end the function
 
-{
-    
-    A *a;
-    
-public:
-    
-    A()
-    
-    {
-        
-        a = this;
-        
-    }
-    
-    
-    bool operator==(const A &obj)
-    
-    {
-        
-        if (&obj == a)
-            
-        {
-            
-            return true;
-            
-        }
-        
-        return false;
-        
-    }
-    
-    
-    bool operator!=(const A &obj)
-    
-    {
-        
-        if (&obj != a)
-            
-        {
-            
-            return true;
-            
-        }
-        
-        return false;
-        
-    }
-    
-};
+void print_stats(vector<int> &v);
+// print out the stats of data that are stored in v
+// includes: max, min, total number of data,
+// and the average
 
-/*
- 
- * Driver program to test the set Class.
- 
- */
+bool is_palindrome(string sentence);
+// return true if the sentence is a palindrome;
+// false otherwise
 
-int main()
-
-{
+int main() {
     
-    A a;
+    // Problem 1
+    vector<int> scores;
+    get_scores(scores);
+    print_stats(scores);
     
-    A b;
-    
-    /* Set of Integers */
-    
-    Set<int> set1;
-    
-    /* Set of Objects */
-    
-    Set<A> set2;
-    
-    /* Adding objects to set */
-    
-    set2.addItem(a);
-    
-    set2.addItem(b);
-    
-    /* Adding items */
-    
-    set1.addItem(5);
-    
-    set1.addItem(6);
-    
-    set1.addItem(1);
-    
-    set1.addItem(1);
-    
-    set1.addItem(10);
-    
-    set1.addItem(12);
-    
-    /* Remove items */
-    
-    set1.removeItem(6);
-    
-    /* Checking members whether members of set or not */
-    
-    set1.IsMember(6);
-    
-    set1.IsMember(5);
-    
-    int * array = set1.getArrayOfSetItems();
-    
-    delete array;
+    // Problem 2
+    string s;
+    cout << "Enter a sentence...\n";
+    cin >> s;
+    getline(cin,s);
+    if(is_palindrome(s))
+        cout << "The sentence: \"" << s << "\" is a palindrome.\n";
+    else
+        cout << "The sentence: \"" << s << "\" is not a palindrome.\n";
     
     return 0;
-    
+}
+
+void get_scores(vector<int> &v){
+    int score;
+    cout << "Please enter list of scores, and enter -1 to stop: \n";
+    do{
+        cin >> score;
+        if(score >= 0)
+            v.push_back(score);
+    } while(score >= 0);
+}
+
+void print_stats(vector<int> &v){
+    int minScore = 101, maxScore = -1, totalScore = 0;
+    size_t size = v.size();
+    for(size_t i = 0; i < size; i++){
+        if(v[i] < minScore)
+            minScore = v[i];
+        if(v[i] > maxScore)
+            maxScore = v[i];
+        totalScore += v[i];
+    }
+    if(size == 0)
+        cout << "There is no test score.\n";
+    else{
+        cout << "There are " << size << " scores.\n";
+        cout << "The highest score is " << maxScore << ".\n";
+        cout << "The lowest score is " << minScore << ".\n";
+        cout << "The average score is " << 1.0 * totalScore/size << ".\n";
+    }
+}
+
+bool is_palindrome(string sentence){
+    int left = 0;
+    int right = (int) sentence.length() - 1;
+    while (left < right){
+        while (!isalpha(sentence[left])){
+            left++;
+            if(left == right)
+                return true;
+        }
+        while (!isalpha(sentence[right])){
+            right--;
+            if(left == right)
+                return true;
+        }
+        if(sentence[left] != toupper(sentence[right]))
+            return false;
+        left++;
+        right--;
+    }
+    return true;
 }
